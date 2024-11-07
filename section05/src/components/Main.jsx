87a060b41f174c6a020553d7d6a894f7e43abe10
yujisoo <jsyu333@gmail.com>
2024-11-07 20:08:57 +0900

    241107 강의내용2

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,3 +1,4 @@
+import "./Main.css"
 // jsx의 주의사항
 // 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다.
 // 2. 숫자, 문자열, 배열 값만 렌더링 된다.
@@ -5,21 +6,28 @@
 // 4. 최상위 태그는 반드시 하나여야만 한다. 빈 태그도 가능  <></>
 
 const Main = () => {
-  const number = 10;
-  const obj = {a : 1}
-  
-  return (
-    <main>
-      <h1>main</h1>
-      <h2>{number  % 2 === 0 ? "짝수" : "홀수"}</h2>
-      {10}
-      {number}
-      {[1,2,3]}
-      {null}
-      {true}
-      {obj.a}
-    </main>
-  )
+  const user = {
+    name: "유지수",
+    isLogin: true, // 로그인 여부
+  }
+
+  // 방법 1
+  if (user.isLogin) {
+    return <div style={{
+      backgroundColor:'red'
+    }}>로그아웃
+      <p className="logout">className</p>
+    </div>
+  }else{
+    return <div>로그인</div>
+  }
+
+  // 방법 2
+  // return (
+  //   <>
+  //     {user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}
+  //   </>
+  // )
 }
 
 export default Main
