046e5b82de076875feda7f77988e52ca65766d38
yujisoo <jsyu333@gmail.com>
2024-11-07 21:13:09 +0900

    241107 강의내용3 추가내용

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Button from './components/Button'
 
 function App() {
+  const buttonProps = {
+    text: "카페",
+    color: "blue",
+    a: 1,
+    b: 2,
+    c: 3
+  }
+
   return (
     <>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      <Button text={"메일"} color={"red"}/>
+      <Button {...buttonProps}/>
+      <Button text={"블로그"}>
+        <div>children</div>
+      </Button>
     </>
   )
 }
