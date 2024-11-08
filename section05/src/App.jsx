b4f947d3db76b703c130e5341e50d82abc92dce3
yujsoo <>
2024-11-08 15:36:44 +0900

    241108 수업내용 - State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,22 +1,26 @@
 import './App.css'
-import Button from './components/Button'
+import { useState } from "react";
 
 function App() {
-  const buttonProps = {
-    text: "카페",
-    color: "blue",
-    a: 1,
-    b: 2,
-    c: 3
-  }
+  //const state = useState();
+  //console.log(state);
+
+  const [count, setCount] = useState(0);
+  //console.log(state,setState)
+  const [light, setLight] = useState("off");
+
 
   return (
     <>
-      <Button text={"메일"} color={"red"}/>
-      <Button {...buttonProps}/>
-      <Button text={"블로그"}>
-        <div>children</div>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={ () => setLight(light === "on" ? "off" : "on")}>{light === "on" ? "끄기" : "켜기"}</button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button type="button" onClick={ () => setCount(count + 1) }>+</button>
+      </div>
+      
     </>
   )
 }
