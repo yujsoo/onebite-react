13dcd63b24c7507a8b4a133e46d8c296225f04ea
yujsoo <>
2024-11-08 17:21:43 +0900

    241108 수업내용 - State로 사용자 입력 관리하기

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -7,54 +7,78 @@ import React, { useState } from 'react'
 // 4. 자기소개
 
 const Register = () => {
-    const [name, setName] = useState("이름");
-    const [birth, setBirth] = useState("");
-    const [country, setCountry] = useState("");
-    const [bio, setBio] = useState("");
+    // 하나의 state로 함께 관리하는 방법
+    const [input, setInput] = useState({
+        name : "",
+        birth : "",
+        country : "",
+        bio : ""
+    })
+    // console.log(input)
 
+    // 통합 이벤트 핸들러
+    const onChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    {/*
     // 생년월일
     const onChangeBirth = (e) => {
-        setBirth(e.target.value);
+        setInput({
+            ...input, // 기존 값을 변경하지 않고 그대로 유지하도록
+            birth : e.target.value 
+        })
     }
 
     // 이름
     const onChangedName = (e) => {
-        //console.log(e.target.value)
-        setName(e.target.value)
+        setInput({
+            ...input, // 기존 값을 변경하지 않고 그대로 유지하도록
+            name : e.target.value 
+        })
     }
 
     // 국적
     const onChangedCountry = (e) => {
-        //console.log(e.target.value)
-        setCountry(e.target.value)
+        setInput({
+            ...input, // 기존 값을 변경하지 않고 그대로 유지하도록
+            country : e.target.value 
+        })
     }
 
     const onChangedBio = (e) => {
-        setBio(e.target.value);
+        setInput({
+            ...input, // 기존 값을 변경하지 않고 그대로 유지하도록
+            bio : e.target.value 
+        })
     }
+    */}
 
     return (
     <div>
         <div>
-            <input value={name} onChange={onChangedName} type="text" placeholder={"이름"} />
-            {name}
+            <input name="name" value={input.name} onChange={onChange} type="text" placeholder={"이름"} />
+            {input.name}
         </div>
         <div>
-            <input value={birth} onChange={onChangeBirth} type="date"/>
-            {birth}
+            <input name="birth" value={input.birth} onChange={onChange} type="date"/>
+            {input.birth}
         </div>
         <div>
-            <select value={country} onChange={onChangedCountry}>
+            <select name="country" value={input.country} onChange={onChange}>
                 <option value=""></option>
                 <option value="kr">한국</option>
                 <option value="us">미국</option>
                 <option value="uk">영국</option>
             </select>
-            {country}
+            {input.country}
         </div>
         <div>
-            <textarea value={bio} onChange={onChangedBio} name="" id=""></textarea>
-            {bio}
+            <textarea name="bio" value={input.bio} onChange={onChange} ></textarea>
+            {input.bio}
         </div>
     </div>
     )
