aeab97b9b11445781c175959e3f1447716a5840c
yujisoo <jsyu333@gmail.com>
2024-11-11 23:44:31 +0900

    241111 수업내용 - React Hooks

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import HookExam from './components/HookExam'
 import Register from './components/Register'
 
 function App() {
   return (
     <>
       <Register></Register>
+      <HookExam></HookExam>
     </>
   )
 }
