7eb252d0e0491ea70ba0b1825ae4355e45a66422
yujsoo <>
2024-11-21 17:18:45 +0900

    241121 수업내용 - 페이지 라우팅3.페이지 이동

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,27 +1,33 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Notfound from './pages/Notfound';
 
-// 페이지를 나눠주기 위해서는 먼저 각각의 페이지 역할을 하게 될 컴포넌트를 먼저 만들어줘야함. -> 페이지도 컴포넌트로 이루어짐!
-// 1. "/" : 모든 일기를 조회하는 home페이지
-// 2. "/new" : 새로운 일기를 작성하는 new페이지
-// 3. "/diary" : 일기를 상세히 조회하는 diary페이지
-
 function App() {
+  // 페이지를 실제로 이동시키는 navigate 함수를 반환해준다.
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav('/new')
+  }
+
   return (
-    <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/new" element={<New></New>}></Route>
-      <Route path="/diary" element={<Diary></Diary>}></Route>
-      <Route path="*" element={<Notfound></Notfound>}></Route>
-      {/*
-        * 와일드카드 스위치케이스문의 디폴트 문과 비슷
-      위에이 ㅆ는 경로에 일치하지않았을떼 이 경로를 실행
-      */}
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동하는 버튼</button>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/new" element={<New></New>}></Route>
+        <Route path="/diary" element={<Diary></Diary>}></Route>
+        <Route path="*" element={<Notfound></Notfound>}></Route>
+      </Routes>
+    </>
   )
 }
 
