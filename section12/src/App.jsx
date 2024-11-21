d3f1f2e8c5ef98ff6083c736bb26d9ea8ad8d78b
yujsoo <jsyu333@gmail.com>
2024-11-21 19:19:52 +0900

    241121 수업내용 - 폰트,이미지,레이아웃 설정하기(1)

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,26 +1,35 @@
 import './App.css'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Notfound from './pages/Notfound';
 
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
+
 function App() {
-  // 페이지를 실제로 이동시키는 navigate 함수를 반환해준다.
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav('/new')
-  }
-
   return (
     <>
+      <div>
+        <img src="./emotion1.png" alt="" />
+        <img src="./emotion2.png" alt=""/>
+      </div>
+      <div>
+        <img src={emotion1} alt=""/>
+        <img src={emotion2} alt=""/>
+        <img src={emotion3} alt=""/>
+        <img src={emotion4} alt=""/>
+        <img src={emotion5} alt=""/>
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
       </div>
-      <button onClick={onClickButton}>New 페이지로 이동하는 버튼</button>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/new" element={<New></New>}></Route>
