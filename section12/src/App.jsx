e876016c73e3f0a1cde600a0913dc791c6813bd7
yujisoo <jsyu333@gmail.com>
2024-11-22 00:48:38 +0900

    241122 수업내용 - 폰트, 이미지, 레이아웃 설정하기(2)

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,26 +4,17 @@ import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Notfound from './pages/Notfound';
-
-import emotion1 from "./assets/emotion1.png";
-import emotion2 from "./assets/emotion2.png";
-import emotion3 from "./assets/emotion3.png";
-import emotion4 from "./assets/emotion4.png";
-import emotion5 from "./assets/emotion5.png";
+import { getEmotionImage } from './util/get-emotion-image';
 
 function App() {
   return (
     <>
       <div>
-        <img src="./emotion1.png" alt="" />
-        <img src="./emotion2.png" alt=""/>
-      </div>
-      <div>
-        <img src={emotion1} alt=""/>
-        <img src={emotion2} alt=""/>
-        <img src={emotion3} alt=""/>
-        <img src={emotion4} alt=""/>
-        <img src={emotion5} alt=""/>
+        <img src={getEmotionImage(1)} alt=""/>
+        <img src={getEmotionImage(2)} alt=""/>
+        <img src={getEmotionImage(3)} alt=""/>
+        <img src={getEmotionImage(4)} alt=""/>
+        <img src={getEmotionImage(5)} alt=""/>
       </div>
       <div>
         <Link to={"/"}>Home</Link>
