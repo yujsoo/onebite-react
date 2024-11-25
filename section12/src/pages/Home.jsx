f1742ac31f815f5d2e9788674337df3e2c1425c0
yujsoo <jsyu333@gmail.com>
2024-11-25 19:24:11 +0900

    241125 수업내용 - Home 페이지 구현하기 2. 기능(1)

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React , { useState } from 'react'
 import Header from '../components/Header'
 import Button from '../components/Button'
 import DiaryList from '../components/DiaryList'
 
 const Home = () => { 
+  const [pivoDate, setPivoDate] = useState(new Date());
+
+  const onIncreaseMonth = () => {
+    setPivoDate(
+      new Date(pivoDate.getFullYear(), pivoDate.getMonth() + 1)
+    );
+  }
+
+  const onDecreaseMonth = () => {
+    setPivoDate(
+      new Date(pivoDate.getFullYear(), pivoDate.getMonth() - 1)
+    )
+  }
+
   return (
     <div>
-      <Header title={"2024년 2월"} leftChild={<Button text={"<"}/>} rightChild={<Button text={">"}/>}/>
+      <Header 
+        title={`${pivoDate.getFullYear()}년 ${pivoDate.getMonth() + 1}월`} 
+        leftChild={<Button text={"<"} onClick={onDecreaseMonth}/>} 
+        rightChild={<Button text={">"} onClick={onIncreaseMonth}/>}/>
       <DiaryList/>
     </div>
   )
